22e3119eeeaee95d61b74776783b4f20b2fa3df5
Thibaut Cens <thibaut.cens@epita.fr>
2016-12-05 15:14:32 +0100

    Exercise 1.1 and 1.2 done.

---
+++ 12-12-2016/src/ex1/eau-total.cc
@@ -0,0 +1,44 @@
+#include <tuple>  //  Lib wich include tuple classes
+#include <vector> //  Lib for vector classes
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#define DATA_PATH_ "data/volumes_d_eau_distribues.txt"
+
+int main()
+{
+  std::ifstream file;
+  try
+  {
+    file.open(DATA_PATH_);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what() << std::endl;
+  }
+
+  //I create a vector containing couples (tuples) which contain a string for
+  //the month and an unsigned for the amount of water used.
+  std::vector<std::tuple<std::string, unsigned>> months;
+
+  while(file.good())
+  {
+    std::string token;
+    file >> token;
+    std::string amount;
+    file >> amount;
+    if (file.good())
+    {
+      months.push_back(std::make_tuple(token, std::stoul(amount)));
+    }
+  }
+
+  long unsigned total  = 0;
+  for(auto i : months)
+  {
+    total += std::get<1>(i);
+  }
+  std::cout << "Paris used " << total
+            << " l of water during the year 2011" << std::endl;
+}
